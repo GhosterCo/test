d2dae03e03c934edcfba722a37324f058befdc01
Nazar <ssteps76@gmail.com>
2024-01-21 17:14:56 +0200

    clear

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
+import Sidebar from "@/components/sidebar/Sidebar";
 import styles from "./page.module.scss";
 
 export default function Home() {
   return (
     <main className={styles.main}>
+      <Sidebar />
       <h1>Home</h1>     
     </main>
   );
